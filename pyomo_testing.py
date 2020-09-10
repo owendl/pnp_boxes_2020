ee6e2c11500519b02e84be34d74ec344c67733e3
Drew Owen <owen.drew.l@gmail.com>
2020-09-10 05:41:39 -0400

    added box ranges constraint, read from a local csv file

--- pyomo_testing.py
+++ pyomo_testing.py
@@ -16,6 +16,8 @@ model = AbstractModel()
 
 df=pd.read_csv("raw.csv")
 
+box_ranges=pd.read_csv("box_ranges.csv")
+
 max_dict=df["Max"].to_dict()
 
 cost_dict = df["Actual_cost"].to_dict()
@@ -24,7 +26,10 @@ flags = pd.melt(df[["Key","Regular","Vegan","GlutenFree"]],  id_vars=["Key"], va
 
 flags_dict = flags.set_index(['Key','variable'])['value'].to_dict()
 
+box_ranges_dict= pd.melt(box_ranges, id_vars="box", value_vars=["max","min"]).set_index(["box","variable"])["value"].to_dict()
+
 boxes = ["Regular","Vegan","GlutenFree"]
+
 
 data={None:
       {
@@ -33,6 +38,7 @@ data={None:
        ,"cost":cost_dict
        ,"max":max_dict
        ,"flags":flags_dict
+       , "box_range":box_ranges_dict
        }
       }
 
@@ -43,6 +49,7 @@ model.pie_idx = Set()
 
 model.box_idx = Set()
 
+model.maxmin_idx=Set(initalize=["max","min"])
 
 
 model.cost = Param(model.pie_idx, within=NonNegativeReals)
@@ -54,8 +61,21 @@ model.flags = Param(model.pie_idx, model.box_idx, within=Binary)
 # model.veg_flag = Param(model.pie_idx, within=Binary)
 # model.glf_flag = Param(model.pie_idx, within=Binary)
 
+
+
 model.pie_box=Var(model.pie_idx, model.box_idx, within=NonNegativeIntegers)
 
 i = model.create_instance(data)
 
 
+
+# Constraint: the total pie bought across multiple boxes is less
+# than the specified max of the pie
+def piemax_rule(model, i,j):
+    return sum(model.pie_box[i,j] for j in model.box_idx) <= model.max[i]
+model.piemax = Constraint( rule=piemax_rule )
+
+# Constraint: number of pies to a box is less than or equal to the number of boxes or zero if the pie is not allowed for that box
+def pieboxmax_rule(model, i,j):
+    return model.pie_box[i,j] <= model.max[i]
+model.pieboxmax = Constraint( rule=pieboxmax_rule )
